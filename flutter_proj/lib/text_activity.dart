e4aeb789c87ffd12dfa2e2e03d07d874088f5b53
Níckolas Javier Santos Lívero <njsl.snf21@uea.edu.br>
2022-11-08 01:46:48 -0400

    função imprimir texto funcionando

--- flutter_proj/lib/text_activity.dart
+++ flutter_proj/lib/text_activity.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_sunmi_printer/flutter_sunmi_printer.dart';
 
 class TextActivity extends StatelessWidget {
   //const MeuApp({Key? key}) : super(key: key);
@@ -82,7 +83,7 @@ class TextActivity extends StatelessWidget {
             ),
             const SizedBox(
               width: 100.0,
-              height: 420.0,
+              height: 220.0,
             ),
             Center(
               child: ClipRRect(
@@ -108,7 +109,61 @@ class TextActivity extends StatelessWidget {
                         padding: const EdgeInsets.all(16.0),
                         textStyle: const TextStyle(fontSize: 20),
                       ),
-                      onPressed: () {},
+                      onPressed: () {
+                        //print('teste0');
+
+                        // Test regular text
+                        SunmiPrinter.hr();
+                        SunmiPrinter.text(
+                          'Test Sunmi Printer',
+                          styles: const SunmiStyles(align: SunmiAlign.center),
+                        );
+                        SunmiPrinter.hr();
+
+                        // Test align
+                        SunmiPrinter.text(
+                          'left',
+                          styles: const SunmiStyles(bold: true, underline: true),
+                        );
+                        SunmiPrinter.text(
+                          'center',
+                          styles:
+                          const SunmiStyles(bold: true, underline: true, align: SunmiAlign.center),
+                        );
+                        SunmiPrinter.text(
+                          'right',
+                          styles: const SunmiStyles(bold: true, underline: true, align: SunmiAlign.right),
+                        );
+
+                        // Test text size
+                        SunmiPrinter.text('Extra small text',
+                            styles: const SunmiStyles(size: SunmiSize.xs));
+                        SunmiPrinter.text('Medium text', styles: const SunmiStyles(size: SunmiSize.md));
+                        SunmiPrinter.text('Large text', styles: const SunmiStyles(size: SunmiSize.lg));
+                        SunmiPrinter.text('Extra large text',
+                            styles: const SunmiStyles(size: SunmiSize.xl));
+
+                        // Test row
+                        SunmiPrinter.row(
+                          cols: [
+                            SunmiCol(text: 'col1', width: 4),
+                            SunmiCol(text: 'col2', width: 4, align: SunmiAlign.center),
+                            SunmiCol(text: 'col3', width: 4, align: SunmiAlign.right),
+                          ],
+                        );
+
+                        // Test image
+
+                        /*
+                        ByteData bytes = await rootBundle.load('assets/rabbit_black.jpg');
+                        final buffer = bytes.buffer;
+                        final imgData = base64.encode(Uint8List.view(buffer));
+                        SunmiPrinter.image(imgData);
+                        */
+
+                        SunmiPrinter.emptyLines(5);
+                        //print('teste1');
+                      },
                       child: const Text('Imprimir'),
                     ),
                   ],
@@ -121,3 +176,10 @@ class TextActivity extends StatelessWidget {
     );
   }
 }
+
+
+
+
+
+
+
